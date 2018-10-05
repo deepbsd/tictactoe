475ec4702ee0f348efba1c979cc40d4e2de6dc52
David S. Jackson <deepbsd@yahoo.com>
2018-10-05 12:21:04 -0400

    changed == to ===

--- main.js
+++ main.js
@@ -27,16 +27,16 @@ $(document).ready(function(){
     var space8 = $("#8").text();
     var space9 = $("#9").text();
     // check rows
-    if      ((space1==space2) && (space2==space3)) { return space3; }
-    else if ((space4==space5) && (space5==space6)) { return space6; }	
-    else if ((space7==space8) && (space8==space9)) { return space9; }
+    if      ((space1===space2) && (space2===space3)) { return space3; }
+    else if ((space4===space5) && (space5===space6)) { return space6; }	
+    else if ((space7===space8) && (space8===space9)) { return space9; }
     // check columns
-    else if ((space1==space4) && (space4==space7)) { return space7; }
-    else if ((space2==space5) && (space5==space8)) { return space8; }
-    else if ((space3==space6) && (space6==space9)) { return space9; }
+    else if ((space1===space4) && (space4===space7)) { return space7; }
+    else if ((space2===space5) && (space5===space8)) { return space8; }
+    else if ((space3===space6) && (space6===space9)) { return space9; }
     // check diagonals
-    else if ((space1==space5) && (space5==space9)) { return space9; }
-    else if ((space3==space5) && (space5==space7)) { return space7; }
+    else if ((space1===space5) && (space5===space9)) { return space9; }
+    else if ((space3===space5) && (space5===space7)) { return space7; }
     // no winner
     return -1;
   }
