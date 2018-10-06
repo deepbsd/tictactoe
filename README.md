3caedd0e2140d1901c9b0dcf6a50c816c0e2fd06
David S. Jackson <deepbsd@yahoo.com>
2018-10-06 15:17:42 -0400

    starting to work on smarter version of tic tac toe game in main2.js

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # tictactoe
 
-I have wanted to do a tictactoe game for a long time.  Just haven't ever made the time to do it.
+This is mainly a prototype for my own experience.  Eventually I want to build this into 
+a one-person game and have the computer be smart enough to play a human.  First is to 
+do the game in jQuery, and once I have the logic part done, I'd like to rewrite the game
+in React.  I'd like to have a backend that computes the logic for each move.
+
+
---
+++ main2.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+	//var origBoard;
+    //let turn = 1;
+    let canplay = true;
+	const huPlayer = 'O';
+	const aiPlayer = 'X';
+
+    let cells = [];
+
+    function getCells(function(){
+        $("td").each(function(){
+            cells.push( $("td").text() );
+        })
+        return cells;
+    })
+
+
+    function checkForWinner(){
+        var space1 = $("#1").text();
+        var space2 = $("#2").text();
+        var space3 = $("#3").text();
+        var space4 = $("#4").text();
+        var space5 = $("#5").text();
+        var space6 = $("#6").text();
+        var space7 = $("#7").text();
+        var space8 = $("#8").text();
+        var space9 = $("#9").text();
+        // check rows
+        if      ((space1===space2) && (space2===space3)) { return space3; }
+        else if ((space4===space5) && (space5===space6)) { return space6; }	
+        else if ((space7===space8) && (space8===space9)) { return space9; }
+        // check columns
+        else if ((space1===space4) && (space4===space7)) { return space7; }
+        else if ((space2===space5) && (space5===space8)) { return space8; }
+        else if ((space3===space6) && (space6===space9)) { return space9; }
+        // check diagonals
+        else if ((space1===space5) && (space5===space9)) { return space9; }
+        else if ((space3===space5) && (space5===space7)) { return space7; }
+        // no winner
+        return -1;
+    }
+	
+	//console.log("cells: ",cells)
+	startGame();
+
+	function startGame() {
+        $("#board tr td").click(function(){
+            if ($(this.text=="" && canplay){
+                if (player === huPlayer) { $(this).append( huPlayer ); }
+                else { $(this).append( aiPlayer ); }
+            } )
+        })
+	}
+
+	function turnClick(square) {
+		turn(square.target.id, huPlayer)
+	}
+
+	function turn(squareId, player) {
+		origBoard[squareId] = player;
+		document.getElementById(squareId).innerText = player;
+	}
+
+
+});
